d104128d495d7144d8023e96f5bdd0124946b344
Kristian <kristiancoulson@gmail.com>
2022-09-23 18:04:41 +0100

    chore: Got scss modules and tailwind to work with storybook

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './button.css';
+import styles from './button.module.scss';
 
 interface ButtonProps {
   /**
@@ -27,21 +27,10 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button type="button" className={`bg-red-500 ${styles.storybookButton}`} style={{ backgroundColor }} {...props}>
       {label}
     </button>
   );
