b0a11b1da64e3e38495eb50ed235c67ef83748c4
Kristian <kristiancoulson@gmail.com>
2022-09-23 17:10:13 +0100

    chore: Install linting and prettier rules

---
+++ src/server/common/get-server-auth-session.ts
@@ -0,0 +1,12 @@
+// Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
+
+import type { GetServerSidePropsContext } from 'next';
+// eslint-disable-next-line camelcase
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions as nextAuthOptions } from '../../pages/api/auth/[...nextauth]';
+
+// Next API route example - /pages/api/restricted.ts
+export const getServerAuthSession = async (ctx: {
+  req: GetServerSidePropsContext['req'];
+  res: GetServerSidePropsContext['res'];
+}) => unstable_getServerSession(ctx.req, ctx.res, nextAuthOptions);
