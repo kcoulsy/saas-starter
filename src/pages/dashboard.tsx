53873dc8a4e092f876872513bab726322a983658
Kristian <kristiancoulson@gmail.com>
2023-02-04 12:36:24 +0000

    chore: Removed unused imports

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,7 @@
 import DashboardNavbar from '@src/components/dashboard/navbar/DashboardNavbar';
 import SettingsTabs from '@src/components/dashboard/settingsTabs/SettingsTabs';
-import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
 
 const Dashboard = () => {
-  const { status } = useSession();
-
   return (
     <div>
       <DashboardNavbar />
