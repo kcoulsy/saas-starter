e89b045a49e350390536553aea1ba8d7bc193643
Kristian <kristiancoulson@gmail.com>
2023-08-16 21:41:07 +0100

    fix: remove console.logs

--- src/app/(auth)/verify/[token]/page.tsx
+++ src/app/(auth)/verify/[token]/page.tsx
@@ -9,16 +9,13 @@ export default async function VerifyPage({ params }: { params: { token: string }
 
   const { token } = params;
 
-  console.log('token', token);
   let success = false;
   try {
     const user = await verifyToken(token);
-    console.log('Verifying token', user, token);
     if (user) {
       success = true;
     }
   } catch (error) {
-    console.log('error', error);
     // Do nothing, redirect
   }
   if (success) {
