55cc04088067e9912a636a8295dc7cde1fc62b4c
Kristian <kristiancoulson@gmail.com>
2023-08-18 08:52:12 +0100

    fix: comment out missing var

--- src/server/services/auth.service.tsx
+++ src/server/services/auth.service.tsx
@@ -93,7 +93,7 @@ export const forgotPassword = async (email: string) => {
 
     if (!user) throw new Error('Email not found');
 
-    const token = jwt.sign({ email, id: user.id }, env.FORGOT_PASSWORD_JWT_SECRET, {
+    const token = jwt.sign({ email, id: user.id }, 'env.FORGOT_PASSWORD_JWT_SECRET', {
       expiresIn: '5m',
     });
 
