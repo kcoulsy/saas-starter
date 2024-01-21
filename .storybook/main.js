64e5238e18c98d11852189d0450ca2cb8aae3c1d
Kristian Coulson <kristiancoulson@gmail.com>
2024-01-21 17:02:54 +0000

    feat: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,21 +6,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    // '@storybook/addon-interactions',
     '@storybook/preset-scss',
-    // 'storybook-addon-next',
-    {
-      /**
-       * Fix Storybook issue with PostCSS@8
-       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
-       */
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
     '@storybook/addon-mdx-gfm',
   ],
   framework: {
